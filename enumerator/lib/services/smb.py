8440cc8c4528a658bff58e170c6ca15cf954d40a
sgabe <sgabe@users.noreply.github.com>
2015-07-25 21:10:59 +0200

    Remove port setting from SMB service module

--- enumerator/lib/services/smb.py
+++ enumerator/lib/services/smb.py
@@ -15,9 +15,9 @@ from ..generic_service import GenericService
 class SmbEnumeration(GenericService, ProcessManager):
     SERVICE_DEFINITION = 'service:netbios-ssn,microsoft-ds or port:139,445'
     PROCESSES = [{
-        'command': 'nmap -sV -Pn -p %(port)s %(scan_mode)s \
+        'command': 'nmap -sV -Pn -p 139,445 %(scan_mode)s \
             --script=smb-os-discovery,smb-system-info,smb-check-vulns,smb-enum-users,smb-enum-shares,smb-enum-processes,smb-enum-sessions,smb-vuln-ms10-061 --script-args=unsafe=1 \
-            -oN %(output_dir)s/%(host)s-smb-%(port)s-standard.txt %(host)s',
+            -oN %(output_dir)s/%(host)s-smb-standard.txt %(host)s',
         'normal': '-T4',
         'stealth': '-T2',
     }]
@@ -35,13 +35,11 @@ class SmbEnumeration(GenericService, ProcessManager):
         """
 
         ip = service_parameters.get('ip')
-        port = service_parameters.get('port')
 
         print '[+] enumerating SMB service on host %s' % ip
         for process in self.PROCESSES:
             self.start_processes(process.get('command'), params={
                 'host': ip,
-                'port': port,
                 'output_dir': directory,
                 'scan_mode': process.get(config.mode),
             }, display_exception=False)
@@ -77,7 +75,7 @@ if __name__ == '__main__':
     Use the following syntax from the root
     directory of enumerator:
 
-    python -m lib.services.smb <ip> <port> <output directory>
+    python -m lib.services.smb <ip> <output directory>
     """
     smb = SmbEnumeration()
-    smb.scan(sys.argv[3], dict(ip=sys.argv[1], port=sys.argv[2]))
+    smb.scan(sys.argv[3], dict(ip=sys.argv[1]))
